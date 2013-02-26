34f94bfbc5d0d0a5ed30685796bc7c954f39cbf6
Min Chen <minchnew@gmail.com>
2013-02-26 16:40:35 +0800

    saturate link: s2 ----- s0, so that when hw pings sn the rtt increases

--- loadbalancer.py
+++ loadbalancer.py
@@ -5,6 +5,8 @@ from mininet.net import Mininet
 from mininet.log import setLogLevel
 from mininet.node import OVSKernelSwitch
 from mininet.cli import CLI
+from mininet.node import CPULimitedHost
+from mininet.link import TCLink
 
 class myTopo(Topo):
 
@@ -14,28 +16,29 @@ class myTopo(Topo):
         Topo.__init__(self, **params)
 
         # Create Clients
-        h_w = self.addHost('h-w')
-        h_s = self.addHost('h-s')
-        h_o1 = self.addHost('h-o1')
-        h_o2 = self.addHost('h-o2')
+        hw = self.addHost('hw', cpu=.05)
+        hs = self.addHost('hs', cpu=.05)
+        ho1 = self.addHost('ho1', cpu=.05)
+        ho2 = self.addHost('ho2', cpu=.05)
 
         # Create servers
-        s_o = self.addHost('s-o')
-        s_n = self.addHost('s-n')
-        s_ne = self.addHost('s-ne')
-        s_se = self.addHost('s-se')
+        so = self.addHost('so', cpu=.2)
+        sn = self.addHost('sn', cpu=.3)
+        sne = self.addHost('sne', cpu=.2)
+        sse = self.addHost('sse', cpu=.1)
 
         # Create switches
         switches = [self.addSwitch('s%d' % i) for i in range(1, 10)]
 
         # Links
-        self.addLink(switches[0], h_w)
-        self.addLink(switches[0], h_o1)
+        self.addLink(switches[0], hw)
+        self.addLink(switches[0], ho1)
         self.addLink(switches[0], switches[1])
-        self.addLink(switches[1], h_o2)
+        self.addLink(switches[1], ho2)
         #self.addLink(switches[1], switches[2])
         self.addLink(switches[1], switches[3])
-        self.addLink(switches[1], switches[4])
+        self.addLink(switches[1], switches[4], bw=100,
+                max_queue_size=10000, use_htb=True)
         #self.addLink(switches[2], switches[3])
         self.addLink(switches[2], switches[4])
         self.addLink(switches[2], switches[5])
@@ -43,18 +46,61 @@ class myTopo(Topo):
         self.addLink(switches[3], switches[6])
         self.addLink(switches[3], switches[7])
         self.addLink(switches[4], switches[8])
-        self.addLink(switches[4], s_o)
-        self.addLink(switches[5], h_s)
-        self.addLink(switches[6], s_se)
-        self.addLink(switches[7], s_ne)
-        self.addLink(switches[8], s_n)
+        self.addLink(switches[4], so)
+        self.addLink(switches[5], hs)
+        self.addLink(switches[6], sse)
+        self.addLink(switches[7], sne)
+        self.addLink(switches[8], sn)
+
+def customNet(**kwargs):
+    topo = myTopo()
+    return Mininet(topo=topo, **kwargs)
+
+def backgroundTraffic(net):
+    so = net.get('so')
+    ho1 = net.get('ho1')
+    ho2 = net.get('ho2')
+
+    so.cmdPrint('iperf -s -w 64K &')
+    ho1.cmdPrint('iperf -c', so.IP(), '-t 3600 -w 64K -P 3 &')
+    ho2.cmdPrint('iperf -c', so.IP(), '-t 3600 -w 64K -P 3 &')
+
+def bgtShut(net):
+    so = net.get('so')
+    ho1 = net.get('ho1')
+    ho2 = net.get('ho2')
+
+    ho1.cmd('kill %iperf')
+    ho2.cmd('kill %iperf')
+    so.cmd('kill %iperf')
+
+def concerningTraffic(net):
+    hw = net.get('hw')
+    hs = net.get('hs')
+    servers = [net.get('sn'), net.get('sne'), net.get('sse')]
+
+    # hs ping Servers
+    print hs.name + ' PING SERVERS'
+    for server in servers:
+        hs.cmdPrint('ping -c4', server.IP())
+
+    # hw ping Servers
+    print hw.name + ' PING SERVERS'
+    for server in servers:
+        hw.cmdPrint('ping -c4', server.IP())
+
+def cntShut(net):
+    pass
 
 if __name__ == '__main__':
     setLogLevel('info')
-    topo = myTopo()
-    net = Mininet(topo=topo, switch=OVSKernelSwitch)
+    net = customNet(host=CPULimitedHost, link=TCLink, switch=OVSKernelSwitch)
     net.start()
-    net.pingAll()
-    CLI(net)
-    net.stop()
 
+    # Background traffic
+    backgroundTraffic(net)
+    #concerningTraffic(net)
+    CLI(net)
+    bgtShut(net)
+
+    net.stop()
